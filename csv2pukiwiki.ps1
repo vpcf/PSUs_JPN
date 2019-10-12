48a18cd112558c86cc5530ceda73ae3fa8abc9b8
vpcf <45737721+vpcf@users.noreply.github.com>
2019-10-12 14:24:26 +0900

    Fix regex string for correct conversion

--- csv2pukiwiki.ps1
+++ csv2pukiwiki.ps1
@@ -13,7 +13,7 @@ $rawtext[0] = $rawtext[0] | ForEach-Object {
        -replace "secondary electrolytic caps brand", "2次側液体Caps" `
        -replace "secondary solid caps brand", "2次側固体Caps" `
        -replace "modular board caps", "プラグイン基板Caps" `
-       -replace "fanless mode", "ファンレス" `
+       -replace "fanless mode", "ﾌｧﾝﾚｽ" `
        -replace "fan", "ファン" `
        -replace "note", "備考" `
        -replace "review/image_", "画像等"
@@ -30,7 +30,7 @@ $content = $rawtext | ForEach-Object {
        -replace "Synchronous Rectification", "同期整流" `
        -replace "Passive Rectification", "ﾀﾞｲｵｰﾄﾞ整流" `
        -replace "Group Regulation", "12V/5V一括制御" `
-       -replace "Group(5V/3.3V) Regulation", "5V/3.3V一括制御" `
+       -replace "Group\(5V/3\.3V\) Regulation", "5V/3.3V一括制御" `
        -replace "鑫", "(金金金)" `
        -replace "九州阳光电源", "九州陽光電源" `
        -replace "japanese", "日本メーカー" `
